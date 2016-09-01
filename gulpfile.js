cd142006ea9df9e7446e44758de9088d45d6319a
Matej Kustec <matej.kustec@gmail.com>
2016-09-01 07:52:08 +0200

    temp

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,22 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
  
  // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./examples"
+        server: "./"
     });
 
-    gulp.watch("./examples/scss/**/*.scss", ['sass']);
-    gulp.watch("./examples/*.html").on('change', browserSync.reload);
+    gulp.watch("./assets/scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("./examples/scss/**/*.scss")
+    return gulp.src("./assets/scss/*.scss")
     .pipe(plumber({
           errorHandler: function(error) {
             console.log(error.message);
@@ -27,7 +28,7 @@ gulp.task('sass', function() {
         }))
     .pipe(sass())
     .pipe(plumber.stop())
-    .pipe(gulp.dest("examples"))
+    .pipe(gulp.dest("assets/css"))
     .pipe(browserSync.stream());
 });
 
